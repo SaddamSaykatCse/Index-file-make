3e23ce69f2ac583c24626148cbb52f2996bc5f7a
SaddamSaykatCse <144375914+SaddamSaykatCse@users.noreply.github.com>
2023-09-22 23:05:31 +0600

    Java Script

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
-console.log(715);
-// console.log(Saddam hossain Saykat);
-console.log("Saykat hasan Saddam");
-console.log(700 - 500);
+// // console.log(715);
+// // console.log(Saddam hossain Saykat);
+// // console.log("Saykat hasan Saddam");
+// // console.log(700 - 500);
+
+// // var age, hasan, mazi;
+// // var nae = saykat;
+// // var image = 20;
+// // var saykat = true;
+// // var Saddam = 55;
+
+// // var fullName = image + Saddam;
+// // var shortName = fullName - Saddam
+// // console.log(shortName / 2);
+
+// // console.log(image);
+// // console.log(Saddam);// console.log(image - Saddam);
+// // var age = 24;
+// // age--;
+// // console.log(age);
+
+// prize1 = '1';
+// prize2 = 2;
+// total_Prize = prize1 + prize2;
+// console.log(total_Prize);
+// var invisible = parseInt(prize1);
+// invisible++;
+// console.log(invisible);
+
+// var mom = 1000;
+// var apple = 700;
+// totalHand = mom - apple;
+// console.log(totalHand);
+
+Math = 75;
+Bio = 25;
+ChE = 65;
+Phy = 80;
+Bag = 35;
+Eng = 45;
+Ict = 99;
+Hm = 50;
+
+totalResult = Math + Bio + ChE + Phy + Bag + Eng + Ict + Hm;
+
+
+average = totalResult / 8;
+
+average = average.toFixed(2);
+
+console.log(average);
+
+
+var mama = 119;
+var sarah = 5;
+console.log(mama % sarah);
+
+var dev = "I am going to be";
+var dev2 = "an awesome web devloper"
+console.log(dev + " " + dev2);
+
+var add = 25;
+add *= 25;
+console.log(add);
+
+var float = 25.0002;
+var float2 = 26.0003;
+
+sum = float + float2;
+
+sum = sum.toFixed(2);
+
+console.log(sum);
+
+var array = [15, 17, 21, 51, 52, 53];
+console.log(array[2]);
+array[4] = 1050;
+console.log(array);
